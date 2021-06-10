bbfcae243f5c67cbcbe83c534677e6b71c092333
Camilo <juancmaringcu@gmail.com>
2021-06-10 18:41:51 -0500

    pasar de JSON a Map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;//Para leer JSON
 
 
@@ -7,16 +9,16 @@ class _MenuProvider{
   List<dynamic> opciones = [];
 
   _MenuProvider() {
-    cargarData();
+    //cargarData();
   }
 
-  cargarData() {
+  Future<List<dynamic>> cargarData() async{
     //Retorna un Future, por lo tanto usamos then
-    rootBundle.loadString('data/menu_opts.json')
-    .then((data) {
-      print(data);
-    });
-
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+    Map dataMap = json.decode(resp);
+    print(dataMap['rutas']);
+    opciones = dataMap['rutas'];
+    return opciones;
   }
 
   
