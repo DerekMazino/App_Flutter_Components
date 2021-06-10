3be4324562b28c7d12cc9601d372e541dacb55d5
Camilo <juancmaringcu@gmail.com>
2021-06-10 17:17:33 -0500

    ListTitle apartir de lista estatica

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeTemp extends StatelessWidget {
-
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,16 +9,24 @@ class HomeTemp extends StatelessWidget {
         title: Text('Componentes temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('List title')
-          ),
-          Divider(),//Genera una linea como división entre items de la lista
-          ListTile(
-            title: Text('List title')
-          ),
-        ],
+        children: _crearItems(),
       ),
     );
+  }
+
+  //Crear una lista de widgets
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = [];
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      //Agregando elementos usando el operador de cascada
+      lista..add(tempWidget)
+          ..add(Divider());
+    }
+
+    return lista;
   }
 }
