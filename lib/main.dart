32fd1acf766ab75ee73b8092ec161acc33619a0c
Camilo <juancmaringcu@gmail.com>
2021-06-12 15:37:23 -0500

    Navegar con rutas con nombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_flutter_components/src/pages/alert_page.dart';
+import 'package:app_flutter_components/src/pages/avatar_page.dart';
 //Mis imports
 import 'src/pages/home_page.dart';
 //import 'package:app_flutter_components/src/pages/home_temp.dart';
@@ -10,7 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Compontes App',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage(),
+      initialRoute: '/', //Indicar la ruta principal
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -42,12 +42,14 @@ class HomePage extends StatelessWidget {
         leading:  getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: (){
-          final route = MaterialPageRoute(
+
+          Navigator.pushNamed(context, opt['ruta']);
+          /* final route = MaterialPageRoute(
             builder: (context) {
               return AlertPage();
             }
           );
-          Navigator.push(context, route);
+          Navigator.push(context, route); */
 
         },
     );
